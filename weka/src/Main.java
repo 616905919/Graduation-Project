1016d3a79fbd327ae42c9611dcae3c8d0e2803e7
ImJosMR <616905919@qq.com>
2016-12-12 17:12:26 +0800

    2016年12月12日17:10:21

--- weka/src/Main.java
+++ weka/src/Main.java
@@ -1,24 +1,30 @@
-import weka.classifiers.functions.MultilayerPerceptron;
-import weka.core.Instances;
-import weka.core.converters.ArffLoader;
-import weka.gui.beans.DataSource;
-
-import java.io.File;
-import java.io.IOException;
-
 /**
  * Created by jiayao on 2016/11/7.
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-        String data = "D:\\design(2)\\design\\data\\4、beast\\beast.csv.arff";
-        ArffLoader arffLoader = new ArffLoader();
-        arffLoader.setFile(new File(data));
-        Instances instances = arffLoader.getDataSet();
-        instances.setClassIndex(instances.numAttributes() - 1);
-        MultilayerPerceptron multilayerPerceptron = new MultilayerPerceptron();
-        multilayerPerceptron.setNominalToBinaryFilter(false);
-        multilayerPerceptron.buildClassifier(instances);
-        multilayerPerceptron.classifyInstance(instances.get(0));
+//        String data = "D:\\design(2)\\design\\data\\4、beast\\beast.csv.arff";
+//        ArffLoader arffLoader = new ArffLoader();
+//        arffLoader.setFile(new File(data));
+//        Instances instances = arffLoader.getDataSet();
+//        instances.setClassIndex(instances.numAttributes() - 1);
+//        MultilayerPerceptron multilayerPerceptron = new MultilayerPerceptron();
+//        multilayerPerceptron.setNominalToBinaryFilter(false);
+//        multilayerPerceptron.buildClassifier(instances);
+//        multilayerPerceptron.classifyInstance(instances.get(0));
+        new Main().hammingWeight((int) 2147483648L);
+    }
+
+//}
+
+    //public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int ans = 0;
+        while (n != 0) {
+            ans += n & 0x1;
+            n = n >>> 1;
+        }
+        return ans;
     }
 }
